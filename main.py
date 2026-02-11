d9103adcd6e3407aafa0e947f38efaf1b34a2db5
freaker2112 <puppersbuddy@gmail.com>
2026-02-11 11:55:55 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+
+lines: {2, }
+
+routes_resp = requests.get('https://svc.metrotransit.org/nextrip/routes', headers={'accept': 'application/json'}).text
+
+
+
+def get_vehicle_position(line):
+    response = requests.get(f'https://svc.metrotransit.org/nextrip/vehicles/{line}', headers={'accept': 'application/json'}).text
+    return response
+    
+
+print(json.dumps(get_vehicle_position(4)))
